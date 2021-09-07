bba1d3142b8a4cc05b38d74192cdfddea1eae3d8
MatisGalvin <matis.galvin@epsi.fr>
2021-09-07 17:13:13 +0200

    front slider

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/styles.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <h1>Bienvenue sur BabyFun !</h1>
+        <div class="container_slide">
+            <div class="slide ">
+                <h2>Attends ton tour en t'amusant !</h2>
+                <p>C'est toi le qui contrôle la partie, à tous moments un vote pour attribuer un malus ou un bonus aux équipes</p>
+            </div>
+            <div class="slide">
+                <h2>Choisissez les <span class="jaune">malus</span> et les <span class="bleu">bonus</span></h2>
+                <div class="intervention">
+                    <div class="bonus">Jouer les mains dans le dos</div>
+                    <div class="bonus">Jouer les mains croisées</div>
+                </div>
+                <div class="intervention">
+                    <div class="bonus" style="background-color: #0098ff;">But compte double</div>
+                    <div class="bonus" style="background-color: #0098ff;">Un jour supplémentaire</div>
+                </div>
+            </div>
+            <div class="slide slide_active">
+                <h2>Attention le temps tourne</h2>
+                <div class="time">
+                    <span>13s</span>
+                </div>
+            </div>
+        </div>
+        <div class="slider_dotte">
+            <div class="dotted dotted_active"></div>
+            <div class="dotted"></div>
+            <div class="dotted"></div>
+        </div>
+        <button class="button_slide">Suivant</button>
+        <p class="foot">Un jeu créé par une équipe de l'EPSI Bordeaux</p>
+    </section>
+</body>
+</html>
