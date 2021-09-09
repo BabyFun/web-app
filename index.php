73d89ad42c4f65f94172eb3223bd1d5fa636e052
antonin187 <antonin.simon@epsi.fr>
2021-09-09 11:04:57 +0200

    'chrono_jauge'

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+require "includes/tools/functions.php";
+require "includes/tools/pickGages.php";
+$gages = getGages();
+$choixBonus = pickGages();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,13 +29,13 @@
                 </div>
                 <div class="intervention">
                     <div class="bonus" style="background-color: #0098ff;">But compte double</div>
-                    <div class="bonus" style="background-color: #0098ff;">Un jour supplémentaire</div>
+                    <div class="bonus" style="background-color: #0098ff;">Un joueur supplémentaire</div>
                 </div>
             </div>
             <div class="slide">
                 <h2>Attention le temps tourne</h2>
                 <div class="time">
-                    <span>13s</span>
+                    <span>10s</span>
                 </div>
             </div>
         </div>
@@ -47,47 +53,83 @@
     <!------------------------   AJOUT ANTO    --------------------------->
 
 
-
-    
     <section class="scoregame" style="display: none;">
         <h1>BabyFun</h1>
         <p class="foot">Un jeu créé par une équipe de l'EPSI Bordeaux</p>
-        <div class="blue_window"><p>03:56</p><p>Temps de jeu</p></div>
+        <div class="blue_window"><p><span id="minutes">00</span>:<span id="seconds">00</span></p><p>Temps de jeu</p></div>
         <div class="scoreboard">
-            <div>5</div>
-            <div>3</div>
+            <div id="score1">0</div>
+            <div id="score2">0</div>
         </div>
         <div class="sharegame">Partage la partie</div>
 
         <button id="change" style="position: absolute; top: 10px">Change la taille</button>
 
+    <?php if($choixBonus[0]['bonus'] == 1) {
 
-
+    ?>
         <div class="malus_popup">
-            <div class="middle_text">
+            <div class="middle_text_bonus">
                 <p>à toi de voter</p>
-                <p>Malus</p>
+                <p>Bonus</p>
             </div>
-            <div class="pledges">
-                <div>Jouer les mains dans le dos</div>
-                <div>Jouer les mains croisées</div>
+            <div class="pledges_bonus">
+                <div><?= $choixBonus[0]['nom'] ?></div>
+                <div><?= $choixBonus[1]['nom'] ?></div>
             </div>
             <div class="gauge_container">
-                <div class="gauge1">
+                <div class="gauge1_bonus">
                     <div><img src="assets/media/images/person.svg"></div>
                     <div>10 personnes</div>
                 </div>
-                <div class="gauge2">
+                <div class="gauge2_bonus">
                     <div><img src="assets/media/images/person.svg"></div>
                     <div>3 personnes</div>
                 </div>
             </div>
-            <div class="time_small">
-                <span>13s</span>
+            <div class="time_small_bonus">
+                <span id="bar-value">&nbsp;</span>
             </div>
 
+        </div>
+
+
+        <div class="middle_text_bonus malus_popup_result">
+            <p>Pour l'équipe *</p>
+            <p>*gage*</p>
+        </div>
+    <?php } else { ?>
+        <div class="malus_popup">
+            <div class="middle_text_malus">
+                <p>à toi de voter</p>
+                <p>Malus</p>
+            </div>
+            <div class="pledges_malus">
+                <div><?= $choixBonus[0]['nom'] ?></div>
+                <div><?= $choixBonus[1]['nom'] ?></div>
+            </div>
+            <div class="gauge_container">
+                <div class="gauge1_malus">
+                    <div><img src="assets/media/images/person.svg"></div>
+                    <div>10 personnes</div>
+                </div>
+                <div class="gauge2_malus">
+                    <div><img src="assets/media/images/person.svg"></div>
+                    <div>3 personnes</div>
+                </div>
+            </div>
+            <div class="time_small_malus">
+                <span id="bar-value">&nbsp;</span>
+            </div>
 
         </div>
+
+
+        <div class="middle_text_malus malus_popup_result">
+            <p>Pour l'équipe *</p>
+            <p>*gage*</p>
+        </div>
+    <?php } ?>
     </section>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="assets/js/script.js"></script>
